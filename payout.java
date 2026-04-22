b52ac4556acbb23be7373058389016edbbd518e3
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-22 22:52:52 +0530

    Add files via upload

---
+++ payout.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class payout {
+
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner (System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if(x >= y){
+		    System.out.println("0");
+		}
+		else{
+		    System.out.println("100");
+		}
+
+	}
+    
+}
