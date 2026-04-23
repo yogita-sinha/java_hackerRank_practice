b0ea118f077f3869d0418c1d4c11803a64b6bd44
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-23 20:51:27 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,15 @@
+import java .util.Arrays;
+import
+public class array2 {
+    public static void main(String args[]){
+        int arr5[]={1,345,256,25,6235,232,345,345,1345,1345,1};
+        int len=arr5.length;
+        System.out.println("length of array is "+len);
+
+        //sorting of array
+        Arrays.sort(arr5);  
+        System.out.println("sorted array is :"+arr5);
+       
+    }
+    
+}
