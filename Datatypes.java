2e27ee493d9e39d687c52a441818f3fb7d2d193b
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-23 20:51:51 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+public class Datatypes {
+    public static void main (String args[]){
+        //primitive datatypes
+        int a = 10; //integer
+        float b = 3.14f; //floating point number
+        char c = 'A'; //character
+        boolean d = true; //boolean
+
+        //non-primitive datatypes
+        String name = "Muskan"; //String
+
+        //output
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + d);
+        System.out.println("String: " + name);
+    }
+    
+}
