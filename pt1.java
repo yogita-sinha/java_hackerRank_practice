aa3e9e96be983ce338e132f481b103479c677693
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-23 20:48:17 +0530

    Add files via upload

---
+++ pt1.java
@@ -0,0 +1,19 @@
+public class pt1 {
+    public static void main(String args[]) {
+
+        //print pattern
+        // *****
+        // *****
+        // *****
+        // *****
+
+        for (int i =1 ;i<=4; i++){
+            for (int j=1; j <=5; j++){
+                System.out.print("*");
+            }
+                System.out.println();
+        }
+      
+    }
+    
+}
