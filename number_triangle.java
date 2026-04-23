ca5233605f929b62efa07257448f69450abc76d6
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-23 20:46:54 +0530

    Add files via upload

---
+++ number_triangle.java
@@ -0,0 +1,19 @@
+public class number_triangle {
+    public static void main(String args[]) {
+        //number triangle
+        //1
+        //12        
+        //123
+        //1234  
+        //12345
+
+        int n=5;
+        for(int i=1; i<=n; i ++){
+            for (int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
