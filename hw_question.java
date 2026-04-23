dad5e52c5ca49c299b557c3136ef7e0abf25a5e1
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-23 20:47:29 +0530

    Add files via upload

---
+++ hw_question.java
@@ -0,0 +1,8 @@
+public class hw_question {
+    //find sum of all arrays 
+    int arr [ ] = {10, 20, 30, 40, 50};
+    // find maxm marks in the  array
+    int arr1 [ ] = {90, 80, 70, 60, 50 , 44};
+     //
+    
+}
