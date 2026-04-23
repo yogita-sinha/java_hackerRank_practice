0d9f1722dfc1c99d48230b7cc73e61fa575245c7
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-23 20:48:39 +0530

    Add files via upload

---
+++ pt2.java
@@ -0,0 +1,25 @@
+public class pt2 {
+    public static void main(String args[]) {
+
+       //hollow rectangle
+       // *****
+       // *   *
+       // *   *
+       // *   *
+       // *****
+
+       int n=5;
+              for (int i= 1; i<=n; i++){
+                for (int j =1; j<=n; j++){
+                    if(i==1||i==n||j==1||j==n){
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+              }
+      
+    }
+    
+}
