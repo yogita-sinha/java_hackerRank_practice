447ae40928cf910e2973ba64354a6ffb10a19cfa
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-26 23:44:23 +0530

    Add files via upload

---
+++ print_numbers.java
@@ -0,0 +1,43 @@
+public class print_numbers {
+    public static void main(String args[]){
+        
+         System.out.println("using for loop");
+         //for loop -> for(initialization; condition; increment/decrement)
+        for(int i=0; i<=10; i++){
+           
+            System.out.println(i );
+        }
+         
+         System.out.println("\nusing while loop");
+        //while loop ->  //initialization
+                        //  while(condition){
+                        //updation  }
+         int j=0;
+         while (j<=10){
+            System.out.println(j);
+            j++;
+
+         }
+
+         
+
+            System.out.println("\nusing do while loop");
+            //do while loop ->  //initialization
+                        // do {
+                       //print
+                        //updation  } 
+                        // while(condition);
+            int k=0;
+            do {
+                System.out.println(k);
+                k++;
+            } while (k<=10);
+
+
+
+
+
+                        }
+
+                        
+    }
