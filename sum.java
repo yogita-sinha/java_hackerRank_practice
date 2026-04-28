bd4dc7a298cef49fbb1ec57e04c9b40a9f0d278d
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-28 22:30:16 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String argd []){
+        int a = 10;
+        int sum=0;
+        for(int i=1; i<=a; i++){
+            sum=sum+i;
+        }
+        System.out.println("Sum of first " + a + " natural numbers is: " + sum);
+    }
+    
+}
