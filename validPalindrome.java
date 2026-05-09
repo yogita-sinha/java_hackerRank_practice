f2eee4872c11b17fd85ab7e20d8d4dd076dc33aa
yogita-sinha <yogita.sinha2606@gmail.com>
2026-05-09 22:39:25 +0530

    Add files via upload

---
+++ validPalindrome.java
@@ -0,0 +1,24 @@
+public class validPalindrome {
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+    
+}
